ff91c585d24bfd53a16be2717dd7559fc8b91ff7
Jean-Marc Vogel <jeanmarc.vogel@gmail.com>
2021-11-05 18:23:31 +0100

    fixup! added FluentCompany

---
+++ FluentHouse/FluentCompany/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FluentCompany
+{
+    public class Employee
+    {
+        public EmploymentContract Contract { get; set; }
+    }
+}
