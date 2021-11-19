d720c4f473de2185a61bc37a46b352e1703e18c6
Jean-Marc Vogel <jeanmarc.vogel@gmail.com>
2021-11-19 17:25:29 +0100

    added TestCustomer

---
+++ FluentTests.Examples/FluentFoo.Tests/Builders/TestCustomer.cs
@@ -0,0 +1,29 @@
+﻿namespace FluentFoo.Tests.Builders
+{
+    public class TestCustomer
+    {
+        private readonly Customer _customer = new();
+
+        public TestCustomer Named(string firstName, string lastName)
+        {
+            _customer.FirstName = firstName;
+            _customer.LastName = lastName;
+            return this;
+        }
+
+        public TestCustomer LivingAt(string address, int zipCode, string town)
+        {
+            _customer.Address = address;
+            _customer.ZipCode = zipCode;
+            _customer.Town = town;
+            return this;
+        }
+
+        public Customer Build() => _customer;
+
+        public static Customer Create() => new TestCustomer()
+            .Named("Hans", "Muster")
+            .LivingAt("Bahnhofstr. 1", 3000, "Bern")
+            .Build();
+    }
+}
---
+++ FluentTests.Examples/FluentFoo/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace FluentFoo
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public int ZipCode { get; set; }
+        public string Town { get; set; }
+    }
+}
