97ff943a2b5e1184759adc4973d3a2eb3484a6db
Jean-Marc Vogel <jeanmarc.vogel@gmail.com>
2021-11-23 10:46:44 +0100

    fix rider warnings

--- FluentTests.Examples/FluentHouse/Program.cs
+++ FluentTests.Examples/FluentHouse/Program.cs
@@ -4,7 +4,7 @@ namespace FluentHouse
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("Hello World!");
         }
