cef3b224791cfc5c31f1a7dd778411e14a5f3fad
Jean-Marc Vogel <jeanmarc.vogel@gmail.com>
2021-11-23 16:10:05 +0100

    code style

--- FluentTests.Examples/FluentHouse/RoomFinder.cs
+++ FluentTests.Examples/FluentHouse/RoomFinder.cs
@@ -13,8 +13,15 @@ namespace FluentHouse
 
         public (int level, string roomName) FindRoomForFriendsMeetup()
         {
-            var largestRoom = _house.Floors.Where(f => f.Level >= 0).SelectMany(f => f.Rooms).OrderByDescending(r => r.Size).First();
-            var level = _house.Floors.Single(f => f.Rooms.Contains(largestRoom)).Level;
+            var largestRoom = _house.Floors.Where(f => f.Level >= 0)
+                .SelectMany(f => f.Rooms)
+                .OrderByDescending(r => r.Size)
+                .First();
+            
+            var level = _house.Floors
+                .Single(f => f.Rooms.Contains(largestRoom))
+                .Level;
+            
             return (level, largestRoom.Name);
         }
     }
